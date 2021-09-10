f9169eb99c07a7c92af416db6afde63ff27892d2
anuragn0507 <71437909+anuragn0507@users.noreply.github.com>
2021-09-10 23:19:40 +0530

    Add files via upload
    10 Aug assignment submit

---
+++ Assignments/Function even or odd/src/com/anurag/Main.java
@@ -0,0 +1,29 @@
+package com.anurag;
+
+import java.util.Scanner;
+
+public class Main {
+
+public static void main(String[] args) {
+        /* this is my 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the number");
+        int num = input.nextInt();
+        evenodd(num);
+        }
+
+
+
+public  static void evenodd(int num){
+        int number = num;
+
+        if (number %2==0) {
+        System.out.println("This is a even number");
+        }
+        else{
+        System.out.println("This is a odd number ");
+        }
+        }
+
+
+}
---
+++ Assignments/methods/src/com/anurag/Main.java
@@ -0,0 +1,12 @@
+package com.anurag;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+
+    Scanner in = new Scanner (System.in);
+    int num1, num2, sum;
+    System.out.print("Enter number 1: ");
+    num1 = in.nextInt();
+}
