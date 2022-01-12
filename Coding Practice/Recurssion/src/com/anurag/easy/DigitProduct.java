9476412945e9a43cec9028f9bc60c6c6560d9dd3
Anurag <anuragn1107@gmail.com>
2022-01-13 04:54:23 +0530

    Recursion Digitsum, Factorial, sum

---
+++ Coding Practice/Recurssion/src/com/anurag/easy/DigitProduct.java
@@ -0,0 +1,19 @@
+package com.anurag.easy;
+
+public class DigitProduct {
+    public static void main(String[] args) {
+        int ans = prod(1342);
+        System.out.println(ans);
+
+    }
+    static int prod(int n){
+        if(n%10 == n){
+            return 0;
+        }
+
+        return (n%10) * prod(n/10);
+
+    }
+
+
+}
---
+++ Coding Practice/Recurssion/src/com/anurag/easy/Sum.java
@@ -0,0 +1,18 @@
+package com.anurag.easy;
+
+public class Sum {
+    public static void main(String[] args) {
+       int ans= sum(5);
+        System.out.println(ans);
+
+    }
+
+    static int sum(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return n+ sum(n-1);
+    }
+}
+
+
