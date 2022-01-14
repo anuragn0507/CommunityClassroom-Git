f33052a26dc56a8bfb7f1a2eb238d06b9f5a9a38
Anurag <anuragn1107@gmail.com>
2022-01-14 15:01:07 +0530

    Commiting Recursion practice questions

---
+++ Coding Practice/Recurssion/src/com/anurag/Main.java
@@ -0,0 +1,20 @@
+package com.anurag;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // recurssion is the most important topic of dsa
+
+        // write a function that prints hello world
+
+        message();
+
+
+    }
+
+    static void message (){
+        System.out.println("happy new year");
+        System.out.println("happy new year again");
+    }
+}
