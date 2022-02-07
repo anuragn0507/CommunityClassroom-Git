91933c090805f623aee5b30062dfb90a19124266
Anurag <anuragn1107@gmail.com>
2022-02-07 13:43:27 +0530

    Commiting all the codes of cc of recursion

---
+++ Coding Practice/Binary Search/primenumber/src/com/anurag/Main.java
@@ -0,0 +1,16 @@
+//package com.anurag;
+//
+//import java.util.Scanner;
+//public class Main {
+//    int in;
+//    public static void main(String[] args) {
+//        Scanner in  = new Scanner(System.in);
+//	  if( in%2 ==0 && in%in==0) {
+//          System.out.println("Is prime number");
+//      }
+//	  else{
+//          System.out.println("Not a prime number");
+//      }
+//    }
+//}
+////Time complexity is n2
