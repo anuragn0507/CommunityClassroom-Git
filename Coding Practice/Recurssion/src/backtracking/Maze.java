215caae94241f674dd40e12ffe366221bab015e7
Anurag <yadavanurag307@gmail.com>
2022-02-12 20:26:59 +0530

    Print path in maze problem including diagonals

--- Coding Practice/Recurssion/src/backtracking/Maze.java
+++ Coding Practice/Recurssion/src/backtracking/Maze.java
@@ -7,6 +7,7 @@ public class Maze {
 //        System.out.println(count(3,3));
         path("", 3,3);
         System.out.println(pathRet("",3,3));
+        System.out.println(pathDiagonal("",3,3));
 
     }
 
@@ -49,6 +50,29 @@ public class Maze {
         }
         if(c > 1) {
             list.addAll(pathRet(p + 'R', r, c - 1));
+        }
+        return list;
+    }
+
+    static ArrayList pathDiagonal(String p, int r, int c) {
+        // Make a note of it when you want to print whole path
+        // then in base case you have to use && both R and C should have 1
+        if (r == 1 && c == 1) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        if(r > 1 && c>1) {
+            list.addAll(pathDiagonal(p + 'D', r - 1, c-1));
+        }
+
+        if(r > 1) {
+            list.addAll(pathDiagonal(p + 'V', r - 1, c));
+        }
+        if(c > 1) {
+            list.addAll(pathDiagonal(p + 'H', r, c - 1));
         }
         return list;
     }
