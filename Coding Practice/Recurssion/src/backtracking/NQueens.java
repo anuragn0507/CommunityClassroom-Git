f8674c07157120e9fb4154a26f725a9c9883af79
Anurag <yadavanurag307@gmail.com>
2022-02-15 10:24:52 +0530

    N queens problem

---
+++ Coding Practice/Recurssion/src/backtracking/NQueens.java
@@ -0,0 +1,18 @@
+package backtracking;
+
+public class NQueens {
+    public static void main(String[] args) {
+
+    }
+
+    static void nQueens(boolean[][] board, int row){
+        if(row == board.length){
+            System.out.println(1);
+            return ;
+        }
+
+
+
+
+    }
+}
